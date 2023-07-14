8f257573af3e22e70bdbf97481d231cd708f4639
GMoftheHamato <jnjuguna635@gmail.com>
2023-07-14 07:39:52 -0700

    unix project

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: 1
+ * main - A program that prints a line without printf or puts
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
